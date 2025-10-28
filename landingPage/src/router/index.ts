f50f3b464a9010a3824219df24f6ae42c988a619
Ariane Helie <arianehelie@gmail.com>
2025-10-28 14:41:39 -0400

    Initial commit

--- landingPage/src/router/index.ts
+++ landingPage/src/router/index.ts
@@ -1,16 +1,16 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
-import HomePage from '../views/HomePage.vue'
+import LandingPage from '../views/LandingPage.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    redirect: '/home'
+    redirect: '/landing-page'
   },
   {
-    path: '/home',
-    name: 'Home',
-    component: HomePage
+    path: '/landing-page',
+    name: 'LandingPage',
+    component: LandingPage
   }
 ]
 
